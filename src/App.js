1e9155d800a902c701c158d3050fdc81bca3d247
jdjinete <jdjinete@gmail.com>
2020-11-12 01:30:03 -0500

    -DJ: First components.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import React from 'react';
+import Header from './components/Header'
+import './default.scss';
+import Homepage from './pages/Homepage'
 
 function App() {
   return (
     <div className="App">
-      <h2>
-        Hello World!
-      </h2>
+      <Header />
+      <Homepage/>
     </div>
   );
 }
---
+++ src/components/Directory/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Directory = props => {
+return (
+    <div>
+
+    </div>
+);
+
+};
+
+export default Directory;
