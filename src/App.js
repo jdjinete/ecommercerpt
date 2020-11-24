6be18c305c544b775c1f38544d26e04b362dbb8a
jdjinete <jdjinete@poli.edu.co>
2020-11-23 22:41:08 -0500

    Implementacion de material design y Carrousel

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Switch, Route } from 'react-router-dom';
 
+
 //layouts
 import MainLayout from './layouts/MainLayout';
 import HomepageLayout from './layouts/HomepageLayout';
--- src/components/Directory/index.js
+++ src/components/Directory/index.js
@@ -1,56 +1,65 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import Radio from '@material-ui/core/Radio';
+import Paper from '@material-ui/core/Paper';
 import Love from './../../assets/amor.jpeg';
 import Dani from './../../assets/Dani.jpg';
 import Sonic from './../../assets/Sonic.jpeg';
 import Romantico from './../../assets/Romantico.png';
+import foto1 from './../../assets/foto1.png';
+import Carousel from 'react-material-ui-carousel'
 import './styles.scss';
 
-const Directory = props => {
-    return (
-        <div className="directory">
-            <div className="wrap">
-               
-                <div
-                    className="item col-sm-4"
-                    style={
-                        {
-                            backgroundImage: `url(${Love})`
-                        }
-                    }>
 
-                   
-                </div>
-                <div
-                    className="item col-sm-4"
-                    style={
-                        {
-                            backgroundImage: `url(${Dani})`
-                        }
-                    }>
-                    <a> Detalles de cumple</a>
-                </div>
-                <div
-                    className="item col-sm-4"
-                    style={
-                        {
-                            backgroundImage: `url(${Sonic})`
-                        }
-                    }>
-                    <a> Detalles para niños</a>
-                </div>
-                <div
-                    className="item col-sm-4"
-                    style={
-                        {
-                            backgroundImage: `url(${Romantico})`
-                        }
-                    }>
-                    <a> Detalles para niños</a>
-                </div>
-            </div>
-        </div>
-    );
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    height: 340,
+    width: 300,
+  },
+  control: {
+    padding: theme.spacing(2),
+  },
+}));
 
-};
+export default function SpacingGrid() {
+  const [spacing, setSpacing] = React.useState(2);
+  const classes = useStyles();
 
-export default Directory;
+  const handleChange = (event) => {
+    setSpacing(Number(event.target.value));
+  };
+
+  
+
+  return (
+      <div class="directorio">
+                   <p class="titulo">
+            Nuestros productos
+            </p>
+      <Grid container className={classes.root} >
+      <Grid item xs={12}>
+        <Grid container justify="center" spacing={spacing}>
+          {[0, 1, 2].map((value) => (
+            <Grid key={value} item>
+              <Paper  className="prueba" elevation={3} 
+                 style={
+                    {
+                        backgroundImage: `url(${foto1})`                        
+                    }
+                }/>
+            </Grid>
+          ))}
+        </Grid>
+      </Grid>
+	   </Grid>
+      </div>
+
+   
+  )}
